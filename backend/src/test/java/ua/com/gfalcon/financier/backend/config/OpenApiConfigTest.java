4d1ef2cb0bf42c9e6df0f9130e5b1c1ade2b52d0
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2021-10-08 23:45:55 +0200

    test(sonar): tests
    Add unit tests

---
+++ backend/src/test/java/ua/com/gfalcon/financier/backend/config/OpenApiConfigTest.java
@@ -0,0 +1,34 @@
+/*
+ *   Copyright 2016-2021 Oleksii V. KHALIKOV, PE
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ua.com.gfalcon.financier.backend.config;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+/**
+ * Tests of {@link OpenApiConfig}.
+ *
+ * @author Oleksii Khalikov
+ */
+public class OpenApiConfigTest {
+
+    @Test
+    public void createOpenApiBean() {
+        assertNotNull((new OpenApiConfig()).openApi());
+    }
+
+}
