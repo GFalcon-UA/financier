6b5991febe17a2b548b6ed2a4419e8607ddb8651
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2023-04-29 20:57:52 +0200

    fix: fix build

--- src/main/java/ua/com/gfalcon/financier/FinancierApplication.java
+++ src/main/java/ua/com/gfalcon/financier/FinancierApplication.java
@@ -3,6 +3,9 @@ package ua.com.gfalcon.financier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Financier application.
+ */
 @SpringBootApplication
 public class FinancierApplication {
 
